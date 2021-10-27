92c6809a64f083f50148bbbaac1569134e3391a7
AbdiGureye <abdifatah.gureye@gmail.com>
2021-10-27 23:48:35 +0000

    Tail.js

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed");
+ }
+  };
+const tail = function(array) {
+  return array.slice(1);
+
+};
+const words = ["One", "two", "three"];
+console.log(words.length);
+tail(words);
+console.log(words.length);
+assertEqual(words.length, 3);
+
