941138927bc9099d5401d6672ed7ada00bdacc2c
AbdiGureye <abdifatah.gureye@gmail.com>
2021-10-30 20:37:19 +0000

    without.js

---
+++ without.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1a, arr1b){
+  if (arr1a.length !== arr1b.length){
+    return false;
+  }
+  for (i = 0; i < arr1a.length; i++) {
+    if (arr1a[i] !== arr1b[i]){
+      return false;
+    }
+  } 
+  return true;
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed ${arr1} === ${arr2}`);
+}
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed ${arr1} !== ${arr2}`);
+  }
+
+};
+
+const without = function (source, itemsToRemove){
+  const newArr = [];
+  
+  for(let element of source) {
+    if(!itemsToRemove.includes(element)) {
+      newArr.push(element)
+    }
+    }
+  
+
+  return newArr
+}
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without([1,5,8], [3]), [1, 5, 8]);
+assertArraysEqual(without(["the", 3, "5"], [1, "the", 5]), (3));
