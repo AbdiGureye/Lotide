358ba6a5a99bbfad818681456b466073a146aff4
AbdiGureye <abdifatah.gureye@gmail.com>
2021-10-30 03:13:08 +0000

    assertArrayEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(arr1a, arr1b){
+  if (arr1a.length !== arr1b.length){
+    return false;
+  }
+  for (i = 0; i < arr1a.length; i++) {
+    if (arr1a[i] !== arr1b[i]){
+      return false;
+    }
+  } 
+  
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed ${arr1} === ${arr2}`);
+}
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed ${arr1} !== ${arr2}`);
+  }
+
+};
