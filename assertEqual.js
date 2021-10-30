164b267b5aec8ed8f03578ae066e4820dbb8c53c
AbdiGureye <abdifatah.gureye@gmail.com>
2021-10-30 01:05:40 +0000

    Added identifier to console.log

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed");
+const assertEqual = function(actual, expected){
+if (actual === expected){
+  console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
+}
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed ${actual} !== ${expected}`);
 
 
   }
   
-};
+}
 
 
-assertEqual("Lighthouse Labs" , "Lighthouse Labs");
-assertEqual("Test", "Testing");
-assertEqual(33, 33);
-assertEqual(30, 23);
+assertEqual ("Lighthouse Labs" , "Lighthouse Labs");
+assertEqual ("Test", "Testing");
+assertEqual (33, 33);
+assertEqual (30, 23)
