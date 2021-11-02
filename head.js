a64218a62341165be52aaf8d7c1ece172d29f3ca
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-02 02:47:31 +0000

    head.js

--- head.js
+++ head.js
@@ -1,20 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed");
-
-
-  }
-};
+const assertEqual = require('./assertEqual');
 const head = function(array){
   console.log(array);
   return array[0]
 }
-assertEqual(head([5,6,7]), 5);
-
-
-assertEqual("Lighthouse Labs" , "Lighthouse Labs");
-assertEqual("Test", "Testing");
-assertEqual(33, 33);
-assertEqual(30, 23);
+module.exports = head;
