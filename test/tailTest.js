abce102d47b63c7e0b82f10a7e238206ad5ec765
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-04 20:15:37 +0000

    tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,19 @@
 const tail = require(`../tail`);
 const assertEqual = require(`../assertEqual`);
+const assert = require('chai').assert;
 
-const words = ["One", "two", "three"];
-console.log(words.length);
-tail(words);
-console.log(words.length);
-assertEqual(words.length, 3);
+describe("#tail", ()=> {
+  it("should return 3 for ['One','two', 'three']", () => {
+    assert.deepEqual(tail(["One", "two", "three"]), ["two", "three"])
+  });
+  it("should return 3 for [5,6, 7,8]", () => {
+    assert.deepEqual(tail([5,6, 7,8]), [6,7,8])
+  });
+
+})
+
+// const words = ["One", "two", "three"];
+// console.log(words.length);
+// tail(words);
+// console.log(words.length);
+// assertEqual(words.length, 3);
