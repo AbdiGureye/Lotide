e98ecc1f015602de897425c9891cdd89bc335685
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-04 20:15:58 +0000

    middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,18 @@
 const middle = require(`../middle`);
 const assertArraysEqual = require(`../assertArraysEqual`)
+const assert = require('chai').assert;
 
-const test = [1, 2, 3, 4] 
-const test1 = [1, 2, 3] 
-assertArraysEqual(middle(test), [2, 3]) // result expected [2, 3]
-assertArraysEqual(middle(test1), [2]) // result expected [2]
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+    it("returns '5' for ['5']", () => {
+      assert.deepEqual(middle(['5']), ['5']); 
+    });
+    
+});
+
+// const test = [1, 2, 3, 4] 
+// const test1 = [1, 2, 3] 
+// assertArraysEqual(middle(test), [2, 3]) // result expected [2, 3]
+// assertArraysEqual(middle(test1), [2]) // result expected [2]
