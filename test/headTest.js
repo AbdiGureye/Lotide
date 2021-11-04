3c91a7fd70c4c586fc1029a643b238cda115462b
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-04 20:16:14 +0000

    headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,22 @@
 const head = require('../head');
 const assertEqual = require(`../assertEqual`);
+const assert = require('chai').assert;
 
 assertEqual(head([5,6,7]), 5);
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  
+    it("returns '5' for ['5']", () => {
+      assert.strictEqual(head(['5']), '5'); 
+    });});
+    it("returns '5' for ['5']", () => {
+      assert.strictEqual(head(['5']), '5'); 
+    });
+});
 
-assertEqual("Lighthouse Labs" , "Lighthouse Labs");
-assertEqual("Test", "Testing");
-assertEqual(33, 33);
-assertEqual(30, 23);
+// assertEqual("Lighthouse Labs" , "Lighthouse Labs");
+// assertEqual("Test", "Testing");
+// assertEqual(33, 33);
+// assertEqual(30, 23);
